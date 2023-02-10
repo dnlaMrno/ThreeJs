068d0540a0fef5b4d710fc2e710c7900a5f2de6f
Daniela Porras <Daniela@Administrators-MacBook-Air.local>
2023-02-10 01:34:25 -0300

    carrito en 3D finalizado

--- src/objects/Car/car.js
+++ src/objects/Car/car.js
@@ -1,5 +1,5 @@
-// import * as TWEEN from '@tweenjs/tween.js/dist/tween.umd' 
-import { Group } from "three";
+import * as TWEEN from '@tweenjs/tween.js/dist/tween.umd';
+import { Group, Object3D } from "three";
 import body from "./body";
 import nose from "./nose";
 import wheel from "./wheel";
@@ -7,31 +7,40 @@ import wheel from "./wheel";
 class car extends Group {
     constructor(){
         super();
+        //CUERPO
         this.Body = new body();
         this.Body.position.set(0, .5, -1);
-        
+        //NARIZ
         this.Nose = new nose();
         this.Nose.position.set(0, 0, .5);
 
+        //RUEDAS
+        this.Wheel_rotation = new Object3D();
         this.Wheel = new wheel();
-        this.Wheel.position.set(0, -.5, 1)
+        this.Wheel_rotation.add(this.Wheel);
+        this.Wheel_rotation.position.set(0, -.5, 1)
+
         this.Wheel2 = this.Wheel.clone()
         this.Wheel2.position.set(1, -.5, -1.5)
+
         this.Wheel3 = this.Wheel.clone()
         this.Wheel3.position.set(-1, -.5, -1.5)
         
-        this.add(this.Body, this.Nose, this.Wheel, this.Wheel2, this.Wheel3);
+        this.add(this.Body, this.Nose,  this.Wheel_rotation, this.Wheel2, this.Wheel3);
 
-        // this.Wheel_tween = new TWEEN.Tween(this.Wheel.rotation)
-        // .to({
-        //     y: .5
-        // })
-        // ,easing(TWEEN.Easing.Sinusoidal.InOut)
-        // .start();
+        this.Wheel_rotation.rotateY(-0.5)
+        this.Wheel_tween = new TWEEN.Tween(this.Wheel_rotation.rotation)
+            .to({
+                y: .5
+            },2000) //Le establezco un tiempo para que vaya mas rapido o mas lento
+            .easing(TWEEN.Easing.Sinusoidal.InOut)
+            .repeat(Infinity)
+            .yoyo(true)
+            .start();
 
     }
     update() {
-        // TWEEN.update();
+        TWEEN.update();
         this.Wheel.rotateY(-0.05);
         this.Wheel2.rotateY(-0.05);
         this.Wheel3.rotateY(-0.05);
